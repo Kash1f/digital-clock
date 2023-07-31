60e0ac8a1e065903408ff61b7b758b037bfcf1c0
Kashif <ahmed.kashif1001@gmail.com>
2023-08-01 02:59:32 +0500

    first-commit

---
+++ Pages/index.js
@@ -0,0 +1,7 @@
+let time = document.getElementById("current-time")
+
+
+setInterval(() => {
+    let date = new Date();
+    time.innerHTML = date.toLocaleTimeString('en', { hour: 'numeric', minute: 'numeric', second:'numeric' , hour24: true});
+},1000)
