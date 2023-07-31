7257bce8b9ce2e9c8c393cd6c17aa8dc2e717471
Kashif <ahmed.kashif1001@gmail.com>
2023-08-01 03:01:26 +0500

    some-changes

--- Pages/index.js
+++ Pages/index.js
@@ -3,5 +3,4 @@ let time = document.getElementById("current-time")
 
 setInterval(() => {
     let date = new Date();
-    time.innerHTML = date.toLocaleTimeString('en', { hour: 'numeric', minute: 'numeric', second:'numeric' , hour24: true});
-},1000)
+    time.innerHTML = date.toLocaleTimeString('en', { hour: 'numeric', minute: 'numeric',second:'numeric' , hour24: true}); },1000)
